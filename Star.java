b522609cbf77169daae5d413a75bae53c1289d1d
Brucegx <guoxi.bruce@gmail.com>
2016-01-19 22:03:12 -0500

    It's just a test for git-flow.

---
+++ Star.java
@@ -0,0 +1,14 @@
+class Star {
+public static void main (String[] args){
+	      int RowNum=1;
+	while (RowNum<=4){
+	   int StarNum=1;
+	while (StarNum<=RowNum){
+	System.out.print("*");
+	StarNum++;
+	}
+	System.out.println();
+	RowNum++;
+	}
+	   }
+	}
